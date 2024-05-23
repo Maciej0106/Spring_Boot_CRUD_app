6c6621710846dbd7f8037006b80a9bf7f438fb5a
Maciej <maciejsikorski57@gmail.com>
2024-05-23 16:15:28 +0200

    Aplikacja do CRUD

---
+++ src/main/java/com/mySpringapp/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,22 @@
+package com.mySpringapp.cruddemo.dao;
+
+import com.mySpringapp.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save (Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
+}
---
+++ src/test/java/com/mySpringapp/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mySpringapp.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
